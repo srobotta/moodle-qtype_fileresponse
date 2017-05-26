f67fa9b4edc9df9a049e8e9760a17ae8e5934c3d
Amr <amr.hourani@id.ethz.ch>
2017-05-26 11:52:54 +0200

    making fileresponse ready for 3.2x

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_fileresponse';
-$plugin->version = 2017032800;
-$plugin->requires = 2015050500;
+$plugin->version = 2017052600;
+$plugin->requires = 2016120502; // moodle 3.2.x
 $plugin->maturity = MATURITY_BETA;
